f97d8d04402fde0f7e2280097bf94e443c6724e9
Peter Juffernbruch <peterjuffernbruch@Peters-MBP.att.net>
2017-12-04 23:13:48 -0600

    all good but counter issue and array display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,19 +4,37 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var guessesSoFar = [];
+var guessesSoFar = [""];
+
+	
 
 document.onkeyup = function(event) {
 
-	var userGuess = event.key;
+	
+
 	var computerTurn = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	 
+	  console.log(computerTurn);
+
+	  var userGuess = event.key;
+
+	
 
 
-	var html =
-		"<div>Wins: " + wins + "</div>"
-		"<div>Losses: " + losses + "</div>"
-		"<div>Guesses Left: " - guessesLeft - "</div>"
-		"<div>Your Guesses so far: " + guessesSoFar + "</div>";
+	if(userGuess === computerTurn) {
+		wins++; alert("You are Psychic!");
+	}
+	else{
+		guessesLeft--;
+	}
+	if(guessesLeft === 0) {
+		losses++; alert("You are NOT Psychic!");
+	}
 
-	document.querySelector("#psychic").innerHTML = html;
-}
+
+	document.getElementById("Wins").innerHTML = "Wins: " + wins;
+	document.getElementById("Losses").innerHTML = "Losses: " + losses;
+	document.getElementById("Left").innerHTML = "Guesses Left: " + guessesLeft;
+	document.getElementById("SoFar").innerHTML = "Your Guesses so far: " + userGuess;
+
+};
