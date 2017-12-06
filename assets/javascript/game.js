3024f27ed1c9cf605905dea808a3c2e0d2834994
Peter Juffernbruch <peterjuffernbruch@Peters-MBP.att.net>
2017-12-06 17:37:18 -0600

    basically finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,37 +4,51 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var guessesSoFar = [""];
+var guessesSoFar = [];
 
-	
 
-document.onkeyup = function(event) {
-
-	
 
 	var computerTurn = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 	 
 	  console.log(computerTurn);
-
-	  var userGuess = event.key;
-
 	
+
+document.onkeyup = function(event) {
+
+
+
+
+	var userGuess = event.key;
+	guessesSoFar.push(userGuess);	
+
+
+
+	var reset = function(computerTurn) {
+		guessesLeft = 9;
+		guessesSoFar = [];
+
+	}
+
 
 
 	if(userGuess === computerTurn) {
-		wins++; alert("You are Psychic!");
+		wins++; alert("You are Psychic! Play again!"); guessesLeft = 9; guessesSoFar.length = 0; computerTurn++;
 	}
+	
 	else{
 		guessesLeft--;
 	}
 	if(guessesLeft === 0) {
-		losses++; alert("You are NOT Psychic!");
+		losses++; alert("You are NOT Psychic! Try again!"); guessesLeft = 9; guessesSoFar.length = 0; computerTurn++;
 	}
+
+
+	
 
 
 	document.getElementById("Wins").innerHTML = "Wins: " + wins;
 	document.getElementById("Losses").innerHTML = "Losses: " + losses;
 	document.getElementById("Left").innerHTML = "Guesses Left: " + guessesLeft;
-	document.getElementById("SoFar").innerHTML = "Your Guesses so far: " + userGuess;
+	document.getElementById("SoFar").innerHTML = "Your Guesses so far: " + guessesSoFar;
 
 };
