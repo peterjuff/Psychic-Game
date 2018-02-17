c20ee9bda9edd9f5aaca3ad4c9df0f2a5fc7106a
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-02-16 23:40:17 -0600

    added letters only catch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,19 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var guessesSoFar = [];
+var guessesSoFar = [ ];
 
 
 
 	var computerTurn = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 	 
 	  console.log(computerTurn);
+
+	  var reset = function(computerTurn) {
+		guessesLeft = 9;
+		guessesSoFar = [ ];
+
+	}
 	
 
 document.onkeyup = function(event) {
@@ -19,36 +25,44 @@ document.onkeyup = function(event) {
 
 
 	var userGuess = event.key;
-	guessesSoFar.push(userGuess);	
+	guessesSoFar.push(" " + userGuess);	
 
 
 
-	var reset = function(computerTurn) {
-		guessesLeft = 9;
-		guessesSoFar = [];
-
+	if (event.keyCode >= 65 && event.keyCode <= 90) {
+		console.log("no numbers");
+	}
+	else {
+		alert("That's not a letter!");
 	}
 
 
 
 	if(userGuess === computerTurn) {
-		wins++; alert("You are Psychic! Play again!"); guessesLeft = 9; guessesSoFar.length = 0; computerTurn++;
+		wins++; alert("You are Psychic! Play again!"); 
+		guessesLeft = 9; 
+		guessesSoFar.length = 0; 
+		computerTurn++;
 	}
 	
 	else{
 		guessesLeft--;
 	}
-	if(guessesLeft === 0) {
-		losses++; alert("You are NOT Psychic! Try again!"); guessesLeft = 9; guessesSoFar.length = 0; computerTurn++;
+	if(guessesLeft == 0) {
+		losses++; 
+		alert("You are NOT Psychic! Try again!"); 
+		guessesLeft = 9; 
+		guessesSoFar.length = 0; 
+		computerTurn++;
 	}
 
 
-	
 
 
 	document.getElementById("Wins").innerHTML = "Wins: " + wins;
 	document.getElementById("Losses").innerHTML = "Losses: " + losses;
 	document.getElementById("Left").innerHTML = "Guesses Left: " + guessesLeft;
-	document.getElementById("SoFar").innerHTML = "Your Guesses so far: " + guessesSoFar;
+	document.getElementById("SoFar").innerHTML = "Your Guesses so far: " + guessesSoFar + "  ";
 
 };
+
